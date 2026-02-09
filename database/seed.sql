eb44a3f654ae430180d099318c418ffb4e3d0066
CrisMollm <cmollmartos@cifpfbmoll.eu>
2026-02-09 16:14:22 +0100

    ajuste de dependencias en el pom.xml para empezar a usar JWT (pom adaptado de la practica angular teniendo en cuenta las diferencias de jdk y base de datos ) y cambios en los datos de prueba para la base de datos

--- database/seed.sql
+++ database/seed.sql
@@ -1,6 +1,6 @@
 -- Usuario de prueba (Password '1234' encriptada)
 INSERT INTO usuario (nombre, email, password, rol) VALUES
-    ('Cristian Moll', 'cristian@test.com', '$2a$10$N.zmdr9k7uOCQb376ye.5.Z95431G5.M5.iE55l5.54848548', 'PACIENTE');
+    ('Cristian Moll', 'cristian@test.com', '1234', 'PACIENTE');
 
 -- Cita de prueba
 INSERT INTO cita (usuario_id, nombre_medico, especialidad, fecha_hora, estado) VALUES
