433365601fd48e383e01999cda0b4a19a542b0bf
CrisMollm <cmollmartos@cifpfbmoll.eu>
2026-02-09 19:16:20 +0100

    creo la clase ValidationExceptionHandler.java para usarla a partir d ahora para gestionar errores

---
+++ backend/src/main/java/com/citasalut/backend/controller/ValidationExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.citasalut.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ValidationExceptionHandler {
+
+    //Se activa con @valid
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        // Recorre todos los campos que fallaron y guarda el motivo
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String field = ((FieldError) error).getField();
+            String msg = error.getDefaultMessage();
+            errors.put(field, msg);
+        });
+
+        // Devuelve un 400 Bad Request con la lista de errores en JSON
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
