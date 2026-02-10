7ffbfbdc95c702e3efd52bc3f809afb425c6a715
CrisMollm <cmollmartos@cifpfbmoll.eu>
2026-02-10 16:19:17 +0100

    creacion del archivo AuthResponse.java para las respustas login/regster con token (autentificación)

---
+++ backend/src/main/java/com/citasalut/backend/dto/AuthResponse.java
@@ -0,0 +1,23 @@
+package com.citasalut.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class AuthResponse {
+
+    private String token;
+    private String type = "Bearer";
+    private String nombre;
+    private String email;
+    private String role;
+
+    public AuthResponse(String token, String type, String nombre, String email, String role) {
+        this.token = token;
+        this.type = type;
+        this.nombre = nombre;
+        this.email = email;
+        this.role = role;
+    }
+
+}
