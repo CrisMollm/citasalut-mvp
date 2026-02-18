767de5de6402931a9e6d61398c89618e2df62b20
CrisMollm <cmollmartos@cifpfbmoll.eu>
2026-02-18 17:10:18 +0100

    guard y cita.service.ts (logica para componente mis-citas)

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -1,23 +1,47 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './components/register/register';
+import {LoginComponent} from './components/login/login';
+import { authInterceptor } from './interceptors/auth.interceptor';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { MisCitas } from './components/mis-citas/mis-citas';
+
+//ANGULAR MATRIAL
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    App
+    App,
+    RegisterComponent,
+    LoginComponent,
+    MisCitas,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
+    AppRoutingModule,
     FormsModule,
-    AppRoutingModule
+    HttpClientModule,
+
+    //Angular Material
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+
   ],
   providers: [
-    provideBrowserGlobalErrorListeners(),
+    provideHttpClient(withInterceptors([authInterceptor]))
   ],
   bootstrap: [App]
 })
--- frontend/src/app/app-routing-module.ts
+++ frontend/src/app/app-routing-module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent} from './components/register/register';
+import { authGuard} from './guards/auth.guards';
+import {LoginComponent} from './components/login/login';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'register', component: RegisterComponent }, // Página de registro
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/register', pathMatch: 'full' }, // Cambiamos el inicio a register
+  { path: '**', redirectTo: '/register' }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
